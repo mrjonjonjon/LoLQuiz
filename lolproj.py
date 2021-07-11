d7718d9492d7cc664b0289ea637cc9db958259f6
mrjonjonjon <86944352+mrjonjonjon@users.noreply.github.com>
2021-07-11 00:11:30 -0700

    Update lolproj.py
    added quotetesting

--- lolproj.py
+++ lolproj.py
@@ -7,6 +7,14 @@ import random
 from PIL import Image
 import requests
 from io import BytesIO
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import ssl
+import re
+
+
+
+
 
 def showimage(champname):
     url = image_dict[champname]
@@ -44,6 +52,16 @@ def testlore():
     else:
         print('incorrect. answer is ', randchampname)
 
+def testquote():
+    randchampname = random.choice(list(lore_dict.keys()))
+    print(quote_dict[randchampname][random.choice(range(0,len(quote_dict[randchampname])))])
+    answer = input("guess the champ:")
+    if answer.lower() == randchampname.lower():
+        print('correct')
+    else:
+        print('incorrect. answer is ', randchampname)
+
+
 lol_watcher = LolWatcher('RGAPI-6046f816-35c0-4de2-a7f0-c463dc877804')
 my_region = 'na1'
 me = lol_watcher.summoner.by_name(my_region, 'XxQuakerOatsxX')
@@ -64,6 +82,7 @@ champ_dict = {}
 lore_dict = {}
 ability_dict = {}
 image_dict={}
+quote_dict={}
 for key in static_champ_list['data']:
     row = static_champ_list['data'][key]
     champ_dict[int(row['key'])] = row['id']
@@ -84,7 +103,54 @@ for key in static_champ_list['data']:
     row = static_champ_list['data'][key]
     image_dict[key] = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+key+'_0.jpg'
 
+ssl._create_default_https_context = ssl._create_unverified_context
+for key in static_champ_list['data']:
+    addtourl=key
+    if addtourl=='AurelionSol':
+        addtourl='Aurelion_Sol'
+    elif addtourl=='Chogath':
+        addtourl='Cho%27Gath'
+    elif addtourl=='DrMundo':
+        addtourl='Dr._Mundo'
+    elif addtourl=='JarvanIV':
+        addtourl='Jarvan_IV'
+    elif addtourl=='Kaisa':
+        addtourl='Kai%27Sa'
+    elif addtourl=='Khazix':
+        addtourl='Kha%27Zix'
+    elif addtourl=='KogMaw':
+        addtourl='Kog%27Maw'
+    elif addtourl=='Leblanc':
+        addtourl='LeBlanc'
+    elif addtourl=='LeeSin':
+        addtourl='Lee_Sin'
+    elif addtourl=='MissFortune':
+        addtourl='Miss_Fortune'
+    elif addtourl=='MasterYi':
+        addtourl='Master_Yi'
+    elif addtourl=='MonkeyKing':
+        addtourl='Wukong'
+    elif addtourl=='Velkoz':
+        addtourl='Vel%27Koz'
+    elif addtourl=='RekSai':
+        addtourl='Rek%27Sai'
+    elif addtourl=='TahmKench':
+        addtourl='Tahm_Kench'
+    elif addtourl=='TwistedFate':
+        addtourl='Twisted_Fate'
+    elif addtourl=='XinZhao':
+        addtourl='Xin_Zhao'
+    url = "https://leagueoflegends.fandom.com/wiki/"+addtourl+"/LoL/Audio"
 
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text()
+    pattern = '["][a-zA-Z. ]*["]'
+    m = re.findall(pattern, text)
+    quote_dict[key] = m
+
+#print(quote_dict)
 
 
 
@@ -114,6 +180,7 @@ while True:
     testability()
     testlore()
     testimage()
+    testquote()
     keep_running = input("Another? (y/n): ")
     if keep_running =='n':
         break
@@ -123,4 +190,9 @@ fix kogmaw vs kog'maw
 fix nunu vs nunu and willlump
 fix mundo vs drmundo
 fix master yi vs masteryi vs yi
+fix tahmkench vs tahm kench
+fix xinzhao vs xin zhao
+kha'zix vs khazix
+morgana and kayle splash show both champs
+nonvoicelines occasionally enclosed in parentheses- e.g "draven laughs"
 '''
